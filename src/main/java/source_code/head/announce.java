02370ab1158bc8fb1c200d2a1e6a77894c6799dd
Arafat2nd <S12027621@STU.Najah.edu>
2022-12-09 18:58:46 +0200

    HEAD

---
+++ src/main/java/source_code/head/announce.java
@@ -0,0 +1,62 @@
+package source_code.head;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class announce {
+
+    @FXML
+    private TextArea text1;
+
+    @FXML
+    void close(ActionEvent event) {
+        Stage t=(Stage) text1.getScene().getWindow();
+        t.close();
+    }
+
+    @FXML
+    void fann(ActionEvent event) throws SQLException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        String oracleUrl = "jdbc:oracle:thin:@localhost:1521/xe";
+        Connection con = DriverManager.getConnection(oracleUrl, "N_LABS", "120120");
+        con.setAutoCommit(false);
+       String sql="select FCAULTY FROM NOUNS";
+         Statement st= con.createStatement();
+        ResultSet rs=st.executeQuery(sql);
+        rs.next();
+        String text=rs.getString(1)+"\n"+dtf.format(now)+": "+text1.getText();
+        String sql1="UPDATE NOUNS SET FCAULTY='"+text+"'";
+        Statement st1= con.createStatement();
+        st1.executeUpdate(sql1);
+        con.commit();
+        con.close();
+        Stage t=(Stage) text1.getScene().getWindow();
+        t.close();
+
+
+    }
+    @FXML
+    void clear(ActionEvent event) throws SQLException {
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        String oracleUrl = "jdbc:oracle:thin:@localhost:1521/xe";
+        Connection con = DriverManager.getConnection(oracleUrl, "N_LABS", "120120");
+        con.setAutoCommit(false);
+          String sql1="UPDATE NOUNS SET FCAULTY='\t'";
+        Statement st1= con.createStatement();
+        st1.executeUpdate(sql1);
+        con.commit();
+        con.close();
+        Stage t=(Stage) text1.getScene().getWindow();
+        t.close();
+     }
+
+
+}
--- src/main/java/source_code/head/home_head.java
+++ src/main/java/source_code/head/home_head.java
@@ -2,16 +2,67 @@ package source_code.head;
 
 import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class home_head {
+import java.io.IOException;
+import java.net.URL;
+import java.security.Timestamp;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class home_head implements Initializable {
+
+    @FXML
+    private TextArea test1;
+
+    @FXML
+    private TextArea text2;
+
+    @FXML
+    private TextArea text3;
     @FXML
     private HBox cardly;
     @FXML
     private HBox cardly1;
+    @FXML
+void show(MouseEvent e) throws SQLException, IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml_head/exp_clk.fxml"));
+        Stage st=new Stage();
+        Parent root = loader.load();
+        st.setScene(new Scene(root));
+        st.show();
 
+}
+@FXML
+void sann(MouseEvent e){
+
+}
+@FXML
+void remann(MouseEvent e){
+
+}
+@FXML
+void remann1(MouseEvent e) throws SQLException {
+    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+    String oracleUrl = "jdbc:oracle:thin:@localhost:1521/xe";
+    Connection con = DriverManager.getConnection(oracleUrl, "N_LABS", "120120");
+    con.setAutoCommit(false);
+    String sql="select FCAULTY FROM NOUNS";
+    Statement st1= con.createStatement();
+    ResultSet rs=st1.executeQuery(sql);
+    rs.next();
+    test1.setText(rs.getString(1));
+}
     public void mouseEnter( ){
         cardly.setScaleX(1);
         cardly.setScaleY(1);
@@ -54,5 +105,47 @@ public class home_head {
         st.setCycleCount((int) 1f);
         st.setAutoReverse(true);
         st.playFromStart();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        String oracleUrl = "jdbc:oracle:thin:@localhost:1521/xe";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(oracleUrl, "N_LABS", "120120");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            con.setAutoCommit(false);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        String sql="SELECT FCAULTY FROM NOUNS ";
+        Statement st= null;
+        try {
+            st = con.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            ResultSet rs= st.executeQuery(sql);
+            rs.next();
+            test1.setText(rs.getString(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
