3be481b5d60bcfe934accfa838bd5622053f818d
Arafat2nd <S12027621@STU.Najah.edu>
2022-12-09 22:47:05 +0200

    HEAD

--- src/main/java/source_code/head/home_head.java
+++ src/main/java/source_code/head/home_head.java
@@ -294,6 +294,5 @@ for(int i=0;i<10;i++){
     lineChart.lookup(".chart-plot-background").setStyle("-fx-background-color: transparent;");
     series.getNode().setStyle("-fx-stroke:#ffffff");
 
-
 }
 }
