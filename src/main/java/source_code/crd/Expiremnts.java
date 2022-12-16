ae5103457f0aff983210d9d0d818dcba1a7dd9d6
Arafat2nd <S12027621@STU.Najah.edu>
2022-12-17 00:22:24 +0200

    inst

--- src/main/java/source_code/crd/Expiremnts.java
+++ src/main/java/source_code/crd/Expiremnts.java
@@ -192,6 +192,7 @@ void sl() throws SQLException {
                 }
                 PopController popController = loader.getController();
                 popController.lab=lab;
+                popController.exp.setText(table.getSelectionModel().getSelectedItem().getName());
                 popController.exp_num=table.getSelectionModel().getSelectedItem().getNumber();
                 popController.name=table.getSelectionModel().getSelectedItem().getName();
                 stage.setScene(new Scene(root));
--- src/main/java/source_code/crd/PopController.java
+++ src/main/java/source_code/crd/PopController.java
@@ -13,10 +13,12 @@ import java.sql.Statement;
 public class PopController {
     public String exp_num;
     public String name;
+    @FXML
+    Label exp;
     String lab;
 @FXML
     void oopen() throws SQLException {
-        String sql="UPDATE EXPIREMNT SET OPEN=1 WHERE num="+exp_num+" AND LAB="+lab;
+        String sql="UPDATE EXPIREMNT SET EXPIREMNT.OPEN=1 where num="+exp_num+" AND LAB="+lab;
         DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
         String oracleUrl = "jdbc:oracle:thin:@localhost:1521/xe";
         Connection con = DriverManager.getConnection(oracleUrl, "N_LABS", "120120");
@@ -57,8 +59,24 @@ public class PopController {
     private Label yt;
 
     @FXML
-    void close(){
-    Stage st=(Stage)yt.getScene().getWindow();
+    void close() throws SQLException {
+        String sql="UPDATE EXPIREMNT SET EXPIREMNT.OPEN=0 where num="+exp_num+" AND LAB="+lab;
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        String oracleUrl = "jdbc:oracle:thin:@localhost:1521/xe";
+        Connection con = DriverManager.getConnection(oracleUrl, "N_LABS", "120120");
+        con.setAutoCommit(false);
+        Statement stmt = con.createStatement();
+        stmt.executeUpdate(sql);
+        con.commit();
+        con.close();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText("Experiment closed successfully");
+        alert.showAndWait();
+
+
+        Stage st=(Stage)yt.getScene().getWindow();
     st.close();
 
     }
